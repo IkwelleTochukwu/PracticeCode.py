c3f8ef2bea00085b0fd2baf4bab2a4fde3a805a5
Ikwelle Tochukwu Anthony <tochiantho99@gmail.com>
2022-09-24 15:28:43 +0100

    Update RegexProject.py

--- RegexProject.py
+++ RegexProject.py
@@ -1,7 +1,8 @@
 #! python3
 # StrongPasswordCheck.
 ''' A function that uses regex to make sure the password string it is passed through user
-user input, is strong enough'''
+user input, is strong enough. A strong password is defined as one that is at 
+least eight characters long, contains both uppercase and lowercase characters, and has at least one digit'''
 
 import re
 StrongPasswordRegex = re.compile(r'[a-z]+[A-Z]+[0-9]+')  # A bug which only allows for strong passwords
