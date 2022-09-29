b5d766a67e7f230cf12e6a60c599e449aa7e4862
IkwelleTochukwu <tochiantho99@gmail.com>
2022-09-29 09:49:03 +0100

    fixed some bugs and added usuage instructions

--- madlibs.py
+++ madlibs.py
@@ -1,15 +1,19 @@
 # A Mad libs program that reads in text files and lets the user add their own
 # text anywhere the word ADJECTIVE, NOUN, ADVERB, or VERB appears in the text file.
+# Usage: Enter the absolute path to the text file in the 'CheckRegex()' function call.
 import re, os
 RegexPattern = re.compile(r'ADJECTIVE|NOUN|ADVERB|VERB')
 def CheckRegex(file_path):
-    if os.path.isfile(file_path):
+    if os.path.isfile(file_path):    # Checks if the given path is a valid file in the system disk
         CheckFile = open(file_path, 'r' )
         CheckContent = CheckFile.read()
         MatchList = RegexPattern.findall(CheckContent)
         for words in MatchList:
-             print(input('Enter your ' + words + ' here\n'))
+            values = input('Enter your ' + words + ' here:\n')
+            CheckContent = CheckContent.replace(words, values, 1)   # replace the file string with the input string
+        print(CheckContent)
     else:
         print('Invalid file path input')
 
-CheckRegex('C:\\Users\\TOCHUKWU IKWELLE\\Documents\\CLOUD DOCS\\myCLOUD-docs\\testinfile.txt')    # Enter the absolute path of your file here
+CheckRegex()    # Enter the absolute path of your file here
+                # note that; for Windows OS path, use the '\\' for partioning and '/' for Linux OS
