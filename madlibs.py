325eb0f1040169e003d19e324f28e5b000e27182
IkwelleTochukwu <tochiantho99@gmail.com>
2023-10-23 17:55:06 +0100

    made the file_path variable to complete the code

--- madlibs.py
+++ madlibs.py
@@ -2,6 +2,7 @@
 # text anywhere the word ADJECTIVE, NOUN, ADVERB, or VERB appears in the text file.
 # Usage: Enter the absolute path to the text file in the 'CheckRegex()' function call.
 import re, os
+file_path = 'C:\\Users\\TOCHUKWU IKWELLE\\Documents\\madlibs.txt'    # File path is a variable 
 RegexPattern = re.compile(r'ADJECTIVE|NOUN|ADVERB|VERB')
 def CheckRegex(file_path):
     if os.path.isfile(file_path):    # Checks if the given path is a valid file in the system disk
@@ -15,5 +16,5 @@ def CheckRegex(file_path):
     else:
         print('Invalid file path input')
 
-CheckRegex()    # Enter the absolute path of your file here
+CheckRegex(file_path)    # Enter the absolute path of your file here
                 # note that; for Windows OS path, use the '\\' for partioning and '/' for Linux OS
